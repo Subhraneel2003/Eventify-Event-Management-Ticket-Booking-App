734d19aa7d280b7210055a6e0911026adc913640
Akshat <akshatjaiswal2518@gmail.com>
2026-07-02 16:17:28 +0530

    modify authSlice

--- eventify/src/store/slices/authSlice.js
+++ eventify/src/store/slices/authSlice.js
@@ -1,33 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  isAuthenticated: false,
   user: null,
   token: null,
-  loading: false,
+  loading: true,
 };
 
 const authSlice = createSlice({
-  name: "auth",
+  name: 'auth',
   initialState,
   reducers: {
-    setLoading: (state, action) => {
-      state.loading = action.payload;
-    },
     login: (state, action) => {
-      state.isAuthenticated = true;
       state.user = action.payload.user;
       state.token = action.payload.token;
       state.loading = false;
     },
     logout: (state) => {
-      state.isAuthenticated = false;
       state.user = null;
       state.token = null;
+      state.loading = false;
+    },
+    completeAuthCheck: (state) => {
       state.loading = false;
     },
   },
 });
 
-export const { setLoading, login, logout } = authSlice.actions;
+export const { login, logout, completeAuthCheck } = authSlice.actions;
 export default authSlice.reducer;
