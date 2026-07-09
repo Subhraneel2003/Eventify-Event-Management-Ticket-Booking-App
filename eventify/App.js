9062925017487fe6e018b3d24980ce54065ae21a
Subhraneel Das <Subhraneel.Das2022@iem.edu.in>
2026-07-09 13:06:51 +0530

    made status bar toggeable using context api

--- eventify/App.js
+++ eventify/App.js
@@ -1,26 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
 import { Provider } from "react-redux";
-import { ThemeProvider } from "./src/context/ThemeContext";
 import { NavigationContainer } from "@react-navigation/native";
+import { ThemeProvider, ThemeContext } from "./src/context/ThemeContext";
 import StackNavigator from "./src/navigation/StackNavigator";
 import store from "./src/store/store";
 import { requestNotificationPermissions } from "./src/services/notificationService";
 
+function AppContent() {
+  const { isDark } = useContext(ThemeContext);
+
+  return (
+    <>
+      <StatusBar
+        style={isDark ? "light" : "dark"}
+      />
+
+      <NavigationContainer>
+        <StackNavigator />
+      </NavigationContainer>
+    </>
+  );
+}
+
 export default function App() {
   useEffect(() => {
     requestNotificationPermissions().catch((err) => {
-      console.log("Error requesting notification permissions on app load:", err);
+      console.log(err);
     });
   }, []);
 
   return (
     <Provider store={store}>
       <ThemeProvider>
-        <NavigationContainer>
-          <StackNavigator />
-        </NavigationContainer>
+        <AppContent />
       </ThemeProvider>
     </Provider>
   );
 }
-
